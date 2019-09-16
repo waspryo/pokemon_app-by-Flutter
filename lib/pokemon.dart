25e5803b638ecf9b2d7ff17145425ffe47760780
Ryo Sugimoto <wasp2123321@gmail.com>
2019-09-16 19:19:18 +0700

    Update pokemon.dart

--- lib/pokemon.dart
+++ lib/pokemon.dart
@@ -111,7 +111,6 @@ class NextEvolution {
   String name;
 
   NextEvolution({this.num, this.name});
-
   NextEvolution.fromJson(Map<String, dynamic> json) {
     num = json['num'];
     name = json['name'];
